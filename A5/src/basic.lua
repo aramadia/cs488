02f6f1dd46b0b0109c1dc564ffcf56f0368f2c48
Daniel Lam <lamdanielalex@gmail.com>
2013-04-08 15:36:58 -0400

    basic.lua

---
+++ A5/src/basic.lua
@@ -0,0 +1,33 @@
+-- An sample array
+
+mat1 = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 25)
+mat2 = gr.material({0.5, 0.5, 0.5}, {0.5, 0.7, 0.5}, 25)
+mat3 = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 25)
+
+scene_root = gr.node('root')
+scene_root:rotate('X', 45)
+
+for x = -3,3 do
+	for y = -3,3 do
+		s1 = gr.nh_sphere('s1', {x * 75, y * 75, -400}, 50)
+		scene_root:add_child(s1)
+		s1:set_material(mat1)		
+
+		b1 = gr.nh_box('b1', {x * 75, y * 75, 0}, 50)
+		scene_root:add_child(b1)
+		b1:set_material(mat3)
+
+ 	end
+end
+
+
+
+white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
+orange_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
+
+-- node, filename, w, h
+-- eye, view, up, fov
+-- ambient, lights
+gr.render(scene_root, 'sample.png', 500, 500, 
+	  {0, 0, 800}, {0, 0, -800}, {0, 1, 0}, 50,
+	  {0.3, 0.3, 0.3}, {white_light, orange_light})
